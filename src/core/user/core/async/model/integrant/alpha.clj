3897cd529c1a8ec796c550be6eb4730afd7f2803
ajchemist <1694505+aJchemist@users.noreply.github.com>
2020-06-16 01:08:56 +0900

    Omitted eastwood test

--- src/core/user/core/async/model/integrant/alpha.clj
+++ src/core/user/core/async/model/integrant/alpha.clj
@@ -4,6 +4,6 @@
    [integrant.core :as ig]
    [taoensso.timbre :as timbre]
    [user.timbre.alpha :as u.timbre]
-   [user.clojure.core.async :refer :all]
+   [user.core.async.alpha :refer :all]
    [user.java.lang.runtime :as java.runtime]
    ))
